ef8306d876a3d7b4bb663eb8dc402e56f754d5a1
foureye1 <utdk256@gmail.com>
2026-05-25 01:54:22 +0530

    temperature converter added

---
+++ cpp-basics/temperature_converter.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){ 
+
+
+//Fahrenheit to  degree Celsius conversion
+
+
+float f, c;
+
+cout<<"enter the temperture in fahrenheit:"<<endl;
+cin>>f;
+
+c = (5.0/9.0)*(f-32);
+
+cout<<"temperature in degree celsius = "<<c<<endl;  
+} 
