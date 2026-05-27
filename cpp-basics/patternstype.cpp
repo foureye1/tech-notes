5b8fef642b5626eec0b79ce6715d32cec233afbe
foureye1 <utdk256@gmail.com>
2026-05-28 02:38:11 +0530

    Added more patterns type

--- cpp-basics/patternstype.cpp
+++ cpp-basics/patternstype.cpp
@@ -192,14 +192,276 @@ while(i<=n) {  // print the character as many time as the input given by user fr
    }  */
 
    
+/*int i = 1;
+char ch = 'A';
+while(i<=n) {
+    int j = 1;
+    while(j<=n) {
+        char ch = 'A'+i+j-2;
+        cout<<ch;
+        j=j+1;
+    } cout<<endl;
+    i=i+1;
+} */
 
+/*int i = 1;
+char ch = 'A';
+while(i<=n){
+    int j = 1;
+    while(j<=i) {
+        char ch = 'A' + i - 1;
+        cout<<ch;
+        j = j+1;
 
+    } cout<<endl;
+    i=i+1;
+} */
+  
 
+// Another pattern 
+/*int i = 1;
+char value = 'A';
+while(i<=n) {
+    int j = 1;
+    while(j<=i) {
+        cout<<value;
+        value=value+1;
+        j=j+1;
+    } cout<<endl;
+    i=i+1;
+} */
 
+//Another pattern program
+
+/*int i = 1;
+char ch = 'A';
+while(i<=n) {
+   int j = 1;
+   while(j<=i) {
+    char ch = 'A'+i+j-2;
+    cout<<ch;
+    j=j+1;
+   }cout<<endl;
+   i=i+1;
+
+} */
+
+//Another pattern program
+
+/* int i = 1;
+char start = 'A'; //define the character 
+while(i<=n) { 
+    int j = 1;
+    char start = 'A' + n - i;  
+    while(j<=i) {
+        cout<< start;
+        start = start + 1;
+        j = j+1;
+    }cout<<endl;
+    i = i+1;
 }
+*/
+//Another pattern program 
+
+/* int i = 1;
+char start = 'A';
+while(i<=n) {
+    int j = 1;
+    while(j<=n) {
+        char start = 'A'+i+j-2;
+        cout<<start;
+        start = start + 1;
+        j = j+1;
+    }cout<<endl;
+    i=i+1;
+}
+*/
+
+// Another pattern program start pattern along with spaces at right side in decreasing order 
+
+/* int i = 1;
+while(i<=n) {
+    int space = n-i;
+    while(space) {
+        cout<<" ";
+        space=space-1;
+    }
+    int j = 1;
+    while(j<=i) {
+        cout<<"*";
+        j = j+1;
+    } cout<<endl;
+    i=i+1;
+} */
+
+
+// homework star pattern
+
+/* int i = 1;
+while(i<=n) {
+    int space = n-i;
+    while(space) {
+        cout<<"*";
+        space=space-1;
+    
+    } cout<<endl;
+    i=i+1;
+} */
+
+
+// another star pattern question 
+
+/* int i = 1;
+while (i<=n) {
+    //spaces
+
+
+    int space = i-1;
+    while(space) {
+         cout<<" ";
+         space = space - 1;
+         }
+   
+//star
+    int star = n-i+1;
+
+    while(star) {
+        cout<<"*";
+        star = star-1;
+    }
+    cout<<endl;
+    i = i + 1;
+
+      } */
+
+
+      // Another pattern problem 
+      /*
+int i = 1;
+while (i<=n) {
+      int space = i-1;
+    while(space) {
+         cout<<" ";
+         space = space - 1;
+         }
+
+         int num = n-i+1;
+         while(num) {
+            cout<<i;
+            num = num - 1;
+            
+            
+         }cout<<endl;
+i=i+1;
+
+        }*/
 
 
 
+        // Another pattern 
+
+        /*int i = 1;
+while (i<=n) {
+      int space = n-i;
+    while(space) {
+         cout<<" ";
+         space = space - 1;
+         }
+
+         int num = i;
+         while(num) {
+            cout<<i;
+            num = num -1;
+            
+            
+         }cout<<endl;
+i=i+1;
+
+        }*/
+
+
+        // Another pattern problem 
+ /* int i = 1;
+while (i<=n) {
+      int space = i-1;
+    while(space) {
+         cout<<" ";
+         space = space - 1;
+         }
+
+         int count = i;
+         while(count<=n) {
+            cout<<count;
+            count = count+1;
+            
+            
+         }cout<<endl;
+i=i+1;
+
+        }*/
+
+
+        //Another pattern problem 
+
+      /*int i = 1;
+    int count = 1;
+
+    while(i <= n) {
+
+        // spaces
+        int space = n - i;
+
+        while(space) {
+            cout << "  ";
+            space = space - 1;
+        }
+
+        // numbers
+        int j = 1;
+
+        while(j <= i) {
+
+            cout << count << " ";
+            count = count + 1;
+
+            j = j + 1;
+        }
+
+        cout << endl;
+        i = i + 1;
+    } */
+
+    int i = 1;
+    while(i<=n) {
+        //numbers
+        int j = 1;
+        while(j<=n-i+1) {
+            cout<<j;
+            j = j+1;
+        } //star
+        int star1 = 1;
+        while(star1<=i-1){
+            cout<<"*";
+            star1 = star1 + 1;
+        }
+        //star
+    int star2=1;
+        while(star2<=i-1){
+            cout<<"*";
+            star2 = star2 + 1;
+        }
+        //number
+        int num = n-i+1;
+        while(num>=1){
+            cout<<num;
+            num=num-1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
+      
+    
 
 
 
